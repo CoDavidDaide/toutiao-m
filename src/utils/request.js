4031eebee1890643c0c6f068e8157ddd23fe15b7
daviddai <233962521@qq.com>
2021-03-20 11:25:26 +0800

    change api url

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const request = axios.create({
-    baseURL: 'http://ttapi.research.itcast.cn/' // 接口基准路径
+    baseURL: 'http://toutiao-app.itheima.net/' // 接口基准路径
 })
 
 // 请求拦截器
